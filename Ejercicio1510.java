cc50aa93c95564336edbae48df16d1e54f17d368
mariaponciano <124848205+mariaponciano@users.noreply.github.com>
2024-10-15 08:58:58 -0600

    Add files via upload

---
+++ Ejercicio1510.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1510;
+import java.io.*;
+/**
+ *
+ * @author m_ponciano
+ */
+public class Ejercicio1510 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
